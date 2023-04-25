088f4c87044927f87f2b155d07e84ab409e722ee
c0b22016c1 <c0b22016c1@edu.teu.ac.jp>
2023-04-25 17:11:47 +0900

    追加１，２途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -33,13 +33,29 @@ def main():
     clock = pg.time.Clock()
     bg_img = pg.image.load("ex02-20230425/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02-20230425/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    #ここまで既存
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)  #引数：(角度、拡大)
+    #追加１(途中)
+    #kk_img = pg.transform.rotozoom(kk_img, 45, 2.0)
+    #kk_imgs = {(0, -1):pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 90, 2.0),
+    #           (+1, -1):pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 45, 2.0),
+    #           (+1, 0):pg.transform.flip(kk_img, True, False),
+    #           (+1, +1):pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 315, 2.0),
+    #           (0, +1):pg.transform.rotozoom(pg.transform.flip(kk_img, True, False), 270, 2.0),
+    #           (-1, +1):pg.transform.rotozoom(kk_img, 45, 2.0),
+    #           (-1, 0):pg.transform.rotozoom(kk_img, 0, 2.0),
+    #           (-1, -1):pg.transform.rotozoom(kk_img, 315, 2.0)}
+    #追加２
+    accs = [a for in range(1,11)]
+    #for r in range(1,11):
+    #    bb_img = pg.Surface((20*r, 20*r))
+    #    pg.draw.circle(bb_img, (255,0,0), (10*r, 10*r), 10*r)
+    #    bb_imgs.append(bb_img)
     kk_rct = kk_img.get_rect()  #ex04
     kk_rct.center = 900, 400
 
     bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img, (255,0,0), (10,10),10 )  #ex01
+    di = 10
+    pg.draw.circle(bb_img, (255,0,0), (10,10), di)  #ex01
     bb_img.set_colorkey((0,0,0))
     x , y = random.randint(0,1600), random.randint(0,900)  #ex02
     #screen.blit(bb_img,[x, y])
@@ -55,7 +71,7 @@ def main():
                 return 0
 
         tmr += 1
-
+        
         #ex04
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
@@ -63,12 +79,18 @@ def main():
                 kk_rct.move_ip(mv)  #move_ip:移動メソッド(引数：横速度、縦速度)
         #ex05
             if check_bound(screen.get_rect(), kk_rct) != (True, True):
-                for k, mv in delta.items():
+                for k, mv in delta.items():  #mv:移動タプル
                     if key_lst[k]:
                         kk_rct.move_ip(-mv[0], -mv[1])
 
+        #追加２的な
+        if tmr % 10 == 0:
+            vx += 1
+            vy += 1
+
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)  #ex04改変
+        #screen.blit(kk_imgs[mv], kk_rct)
         bb_rct.move_ip(vx,vy)  #ex03
         yoko, tate = check_bound(screen.get_rect(), bb_rct)
         if not yoko: #横に出てる
